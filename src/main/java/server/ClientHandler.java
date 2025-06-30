3b1ad2bbd2356a607e21380b219fe660d68979f6
royal <royalhellt@gmail.com>
2025-06-30 14:25:16 +0500

    Выполнена базовая реализация многопользовательского чат-сервера.
    Для этого, был обновлен класс ClientHandler.java. Добавлен потокобезопасной метод broadcast(), который уведомляет всех активных клиентов о сообщении других клиентов в едином чате.

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -5,31 +5,45 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientHandler implements Runnable {
     private Socket socket;
+    private PrintWriter out;
+
+    private static final List<PrintWriter> clients = Collections.synchronizedList(new ArrayList<>());
 
     public ClientHandler(Socket socket) {
         this.socket = socket;
     }
 
+    private void broadcast(String message) {
+        synchronized (clients) {
+            for (PrintWriter client : clients) {
+                client.println(message);
+            }
+        }
+    }
+
     @Override
     public void run() {
         try (
-                InputStream is = socket.getInputStream();
-                OutputStream os = socket.getOutputStream();
-                Scanner scanner = new Scanner(is);
-                PrintWriter out = new PrintWriter(os, true)
-        )
-        {
+                InputStream inputStream = socket.getInputStream();
+                OutputStream outputStream = socket.getOutputStream();
+                Scanner input = new Scanner(inputStream);
+        ) {
+            out = new PrintWriter(outputStream, true);
+            clients.add(out);
             out.println("Добро пожаловать в чат!");
-            while (scanner.hasNextLine()) {
-                String message = scanner.nextLine();
-                System.out.println("Получено сообщение: " + message);
-                out.println("Эхо: " + message);}
-        }
 
+            while (input.hasNextLine()) {
+                String message = input.nextLine();
+                broadcast("Клиент " + socket.getPort() + ": " + message);
+            }
+        }
         catch (IOException exception) {
             System.err.println("Клиент отключился: " + exception.getMessage());}
 
